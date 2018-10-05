af11230cacbe61d398907431ef8be2d4b10b4633
Honza Javorek <mail@honzajavorek.cz>
2018-10-05 13:33:14 +0200

    chore: forget about latest vs. stable
    Dredd does not use the tags anymore and it's there only for historical
    purposes. Let's not complicate our lives, $(npm view dredd version) is
    just fine.

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-DREDD_VERSION=`./scripts/getLatestStableNodePackage dredd`
+#!/bin/bash
+DREDD_VERSION=$(npm view dredd version)
 echo "Building version: $DREDD_VERSION"
-docker build -t apiaryio/dredd:$DREDD_VERSION --build-arg DREDD_VERSION="$DREDD_VERSION" -f Dockerfile --no-cache .
-docker tag apiaryio/dredd:$DREDD_VERSION apiaryio/dredd:latest
+docker build \
+  -t "apiaryio/dredd:$DREDD_VERSION" \
+  --build-arg DREDD_VERSION="$DREDD_VERSION"
+  -f Dockerfile \
+  --no-cache \
+  .
+docker tag "apiaryio/dredd:$DREDD_VERSION" apiaryio/dredd:latest
--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DREDD_VERSION=`./scripts/getLatestStableNodePackage dredd`
+DREDD_VERSION=$(npm view dredd version)
 echo "Building version: $DREDD_VERSION"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push apiaryio/dredd:$DREDD_VERSION
+docker push "apiaryio/dredd:$DREDD_VERSION"
 docker push apiaryio/dredd:latest
