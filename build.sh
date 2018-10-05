ce2f63a181203f433cb1b95d14d074831e4b7dc0
Honza Javorek <mail@honzajavorek.cz>
2018-10-05 14:26:49 +0200

    chore: fix line wrapping

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@ DREDD_VERSION=$(npm view dredd version)
 echo "Building version: $DREDD_VERSION"
 docker build \
   -t "apiaryio/dredd:$DREDD_VERSION" \
-  --build-arg DREDD_VERSION="$DREDD_VERSION"
-  -f Dockerfile \
-  --no-cache \
-  .
+  --build-arg DREDD_VERSION="$DREDD_VERSION" \
+  -f Dockerfile --no-cache .
 docker tag "apiaryio/dredd:$DREDD_VERSION" apiaryio/dredd:latest
 docker tag "apiaryio/dredd:$DREDD_VERSION" apiaryio/dredd:stable
